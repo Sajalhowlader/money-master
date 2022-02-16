f0567db0b95852cf85be80bca8bdc278cc4d2304
Sajalhowlader <sajolhowlader08@gmail.com>
2022-02-16 22:55:44 +0600

    update total blance

--- javascript/main.js
+++ javascript/main.js
@@ -3,8 +3,15 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
     const foodRent = document.getElementById('food-input').value;
     const othersRent = document.getElementById('others-input').value;
     const totalExpensePrice = parseFloat(houseRent) + parseFloat(foodRent) + parseFloat(othersRent)
-    console.log(totalExpensePrice)
+
     const totalPrice = document.getElementById('total-expense')
+    totalPrice.innerText = totalExpensePrice;
+    const income = document.getElementById('income')
+    const incomeTotal = income.value;
+    const blanceTotal = parseFloat(incomeTotal) - parseFloat(totalExpensePrice)
+    const totalBlance = document.getElementById('total-blance')
+    totalBlance.innerText = blanceTotal;
+
 
 
 })
