f170c755451008eb85fa7ef6d3226cc25321d6c5
Sajalhowlader <sajolhowlader08@gmail.com>
2022-02-17 19:57:43 +0600

    add new condition

--- javascript/main.js
+++ javascript/main.js
@@ -1,19 +1,19 @@
-// Incone input an income value
+// Income input id
 const income = document.getElementById('income')
 
-// house rent input and house rent value
+// house rent input id
 const houseRent = document.getElementById('house-input');
 
-// food rent input and food rent value
+// food rent input id
 const foodRent = document.getElementById('food-input')
 
-// others rent value and others rent input
+// others rent input id
 const othersRent = document.getElementById('others-input');
 
-// total expense 
+// total expense id
 const expense = document.getElementById('total-expense')
 
-// total blance after total cost
+// total blance id
 const blance = document.getElementById('total-blance')
 
 // save money input field
@@ -27,15 +27,21 @@ const remaining = document.getElementById('remaining-blance')
 
 
 document.getElementById('calculate-btn').addEventListener('click', function () {
+    //  sum in total cost house rent foodrent and others
+    const totalExpense = parseFloat(houseRent.value) + parseFloat(foodRent.value) + parseFloat(othersRent.value);
+    // if expense is bigger than income it will give a alert
+    if (income.value < totalExpense) {
+        document.getElementById('expense').style.display = "block"
+        document.getElementById('denger').style.display = "block"
 
-    if (income.value > 0 && houseRent.value >= 0 && foodRent.value >= 0 && othersRent.value >= 0) {
-        const totalExpense = parseFloat(houseRent.value) + parseFloat(foodRent.value) + parseFloat(othersRent.value);
+    } else if (income.value >= 0 && houseRent.value >= 0 && foodRent.value >= 0 && othersRent.value >= 0) {
 
         expense.innerText = totalExpense
-
+        // total blance >> income - expense
         const totalBlance = parseFloat(income.value) - parseFloat(totalExpense)
 
         blance.innerText = totalBlance
+
     } else {
         document.getElementById('denger').style.display = "block"
     }
@@ -44,11 +50,13 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
 })
 
 document.getElementById('save-btn').addEventListener('click', function () {
+
     const totalExpense = parseFloat(houseRent.value) + parseFloat(foodRent.value) + parseFloat(othersRent.value);
+
     const totalBlance = parseFloat(income.value) - parseFloat(totalExpense)
     console.log(totalBlance)
 
-    if (totalBlance > saveingAmount.innerText) {
+    if (totalBlance > saveInput.value) {
 
         const saveMoney = (income.value * saveInput.value) / 100;
         saveingAmount.innerText = saveMoney;
@@ -58,7 +66,7 @@ document.getElementById('save-btn').addEventListener('click', function () {
         remaining.innerText = remainingBlance
 
     } else {
-
+        document.getElementById('no-amount').style.display = "block"
     }
 
 
