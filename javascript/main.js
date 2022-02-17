2e42bc483b4f3e6e0b14f3153e72e511712c59a0
Sajalhowlader <sajolhowlader08@gmail.com>
2022-02-17 08:41:47 +0600

    add save money

--- javascript/main.js
+++ javascript/main.js
@@ -2,16 +2,35 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
     const houseRent = document.getElementById('house-input').value;
     const foodRent = document.getElementById('food-input').value;
     const othersRent = document.getElementById('others-input').value;
+    // intotal expense price house food and others
     const totalExpensePrice = parseFloat(houseRent) + parseFloat(foodRent) + parseFloat(othersRent)
 
     const totalPrice = document.getElementById('total-expense')
     totalPrice.innerText = totalExpensePrice;
     const income = document.getElementById('income')
+
     const incomeTotal = income.value;
+    // blance total net income minus total expense
     const blanceTotal = parseFloat(incomeTotal) - parseFloat(totalExpensePrice)
+
     const totalBlance = document.getElementById('total-blance')
     totalBlance.innerText = blanceTotal;
 
+
+
+})
+
+document.getElementById('save-btn').addEventListener('click', function () {
+    const saveInput = document.getElementById('save-input').value
+    const income = document.getElementById('income').value
+    const saveMoney = (income * saveInput) / 100
+    const saveingAmount = document.getElementById('saveing-amount')
+    saveingAmount.innerText = saveMoney;
+
+    const totalBlance = document.getElementById('total-blance').innerText
+    const remaingMoney = totalBlance - saveMoney;
+    const setRemainingMoney = document.getElementById('remaining-blance')
+    setRemainingMoney.innerText = remaingMoney
 
 
 })
